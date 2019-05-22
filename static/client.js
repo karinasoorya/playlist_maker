ab0b451e2260f3d41626bd2574ec81ea4775f0af
karinasoorya <karina.soorya@gmail.com>
2019-05-22 17:46:51 -0400

    Delete client.js

--- static/client.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $("img.image_selector").click(function () {
-      var id = this.id;
-      $.get('/put_param/' + id, function(thing) {
-          window.location.href = "http://localhost:8000/audio_features/" + id;
-      });
-    });
-});
